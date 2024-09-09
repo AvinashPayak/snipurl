7020bcd322226b6431b025c0e8993593090312f3
Avinash Payak <avinashpayak@gmail.com>
2024-09-10 00:39:22 +0530

    dashboard complete

---
+++ components/LinkItem.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+defineProps<{
+  link: {
+    shortKey: string;
+    longUrl: string;
+    id: string;
+  };
+}>();
+</script>
+
+<template>
+  <section class="container m-10">
+    <div class="card flex justify-between">
+      <div class="link-info">
+        <div class="text-purple-500 font-bold text-2xl">
+          {{ link.shortKey }}
+        </div>
+        <div class="text-sm text-white/50">
+          {{ link.longUrl }}
+        </div>
+      </div>
+      <button
+        class="btn-primary w-12 h-12 grid place-content-center rounded-full"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+          />
+        </svg>
+      </button>
+    </div>
+  </section>
+</template>
+
