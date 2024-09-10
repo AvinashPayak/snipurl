d3976dbee715a303184533aa5d7d5e7725f37012
Avinash <prernabha98@gmail.com>
2024-09-10 12:39:32 +0530

    dashboard link shortner form logic added

--- components/UrlForm.vue
+++ components/UrlForm.vue
@@ -1,15 +1,15 @@
 <template>
     <section class="container mt-10">
       <div class="card">
-        <form>
+        <form @submit.prevent="handleUrlForm">
           <div class="flex gap-5 items-end">
             <div class="w-5/12">
               <label for="url" class="text-white">Long URL</label>
-              <input type="text" name="url" id="url" />
+              <input type="text" name="url" id="url" v-model="form.url" />
             </div>
             <div class="w-5/12">
               <label for="shortkey" class="text-white">Short Key</label>
-              <input type="text" name="shortkey" id="shortkey" />
+              <input type="text" name="shortkey" id="shortkey" v-model="form.shortkey" />
             </div>
             <div class="w-2/12">
                 <button class="btn-primary py-3 rounded-xl w-full">Short</button>
@@ -19,3 +19,42 @@
       </div>
     </section>
 </template>
+
+<script lang="ts" setup>
+import { nanoid } from "nanoid";
+import type { Database } from "~/database.types";
+const form = reactive({
+  url:"",
+  shortkey:""
+})
+const client = useSupabaseClient<Database>()
+const user = useSupabaseUser()
+
+const handleUrlForm = async () => {
+  try {
+    const { data, error } = await client.from('links').insert({
+      long_url: form.url,
+      key: form.shortkey,
+      user_id: user.value?.id
+    })
+
+    if(error) {
+      alert(error.message)
+      return
+    }
+
+      generateShortkey()
+      form.url = ""
+      alert("link created")
+
+  } catch(error) {
+    console.log("Error:", error)
+  }
+}
+const generateShortkey = (length:number = 6): void => {
+  form.shortkey = nanoid(length)
+}
+onMounted(() => {
+  generateShortkey()
+})
+</script>
