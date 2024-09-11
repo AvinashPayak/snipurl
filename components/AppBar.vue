364f7def18e8100cd1ad402a41b2d867a6baf38c
Avinash <avinashpayak@gmail.com>
2024-09-11 11:45:46 +0530

    logout complete

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -16,6 +16,14 @@ const navLinks = ref<{
         label: "Contact",
     }
 ])
+
+const user = useSupabaseUser()
+const client = useSupabaseClient()
+const handleLogout = () => {
+    client.auth.signOut()
+
+    useRouter().push({ name: 'index'})
+}
 </script>
 
 <template>
@@ -26,7 +34,8 @@ const navLinks = ref<{
             }" class="text-2xl font-bold text-white">SnipURL</NuxtLink>
             <ul class="flex items-center gap-4">
             <li v-for="navLink in navLinks" :key="navLink.to"><NuxtLink :to="navLink.to">{{ navLink.label }}</NuxtLink></li>
-            <li class="btn btn-primary"><NuxtLink to="/auth">Sign In</NuxtLink></li>
+            <li v-if="!user" class="btn btn-primary"><NuxtLink to="/auth">Sign In</NuxtLink></li>
+            <li v-else class="btn btn-primary" @click="handleLogout">Sign out</li>
         </ul>
         </nav>
     </div> 
